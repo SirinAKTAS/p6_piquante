0fce2d70811e234f362420c2ad91a44d2ec1e451
Sirin Aktas <sirinaktas@live.Fr>
2022-08-26 19:49:28 +0200

    second step in progress

--- back/app.js
+++ back/app.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
-const app = express();
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
 
 mongoose.connect('mongodb+srv://sirin:user123@cluster0.dlzt1wy.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -18,11 +10,19 @@ mongoose.connect('mongodb+srv://sirin:user123@cluster0.dlzt1wy.mongodb.net/?retr
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a été reçue !' }); 
+const app = express();
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
-app.use('http://localhost:3000//api/auth', userRoutes);
+app.use(bodyParser.json());
+
+app.use('/api/auth', userRoutes);
 
 
 module.exports = app;
