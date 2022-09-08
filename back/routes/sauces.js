d20591505fe448dc42c039f2f5c7ff5a4e901fe9
Sirin Aktas <sirinaktas@live.Fr>
2022-09-08 14:26:53 +0200

    validate-inputs

--- back/routes/sauces.js
+++ back/routes/sauces.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
-
+const validate = require('../middleware/validate-inputs');
 const saucesCtrl = require('../controllers/sauces');
 
 router.get('/', auth, saucesCtrl.getAllSauces);
-router.post('/', auth, multer, saucesCtrl.createSauce);
-router.get('/:id', auth, saucesCtrl.getOneSauce);  
-router.put('/:id', auth, multer, saucesCtrl.modifySauce);
-router.delete('/:id', auth, saucesCtrl.deleteSauce);
-router.post('/:id/like', auth, saucesCtrl.likeSauce);
+router.post('/', auth, multer, validate.sauce, saucesCtrl.createSauce);
+router.get('/:id', auth, validate.id, saucesCtrl.getOneSauce);  
+router.put('/:id', auth, multer, validate.id, validate.sauce, saucesCtrl.modifySauce);
+router.delete('/:id', auth, validate.id, saucesCtrl.deleteSauce);
+router.post('/:id/like', auth, validate.id, validate.like, saucesCtrl.likeSauce);
 
 module.exports = router;
--- back/routes/user.js
+++ back/routes/user.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
-
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+const validate = require('../middleware/validate-inputs');
+router.post('/signup', validate.user, userCtrl.signup);
+router.post('/login', validate.user, userCtrl.login);
 
 module.exports = router;
